9b01d60a81f20bac2ecb77d6f9f5d7fd4b232a15
MarianaDobrivska <dobrivska.marry@gmail.com>
2023-01-13 11:29:05 +0200

    remove excessive comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,17 @@
-import { useEffect, lazy } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { PublicRoute } from './PublicRoute';
 import { PrivateRoute } from './PrivateRoute';
 import { SignUpPage } from 'pages/Registration/RegisterPage';
 import { LogInPage } from 'pages/LogIn/LogInPage';
-import { getIsFetchingCurrent } from 'redux/auth/authSelectors';
 import { PhoneBookPage } from 'pages/Contacts/PhonebookPage';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from 'pages/HomePage/HomePage';
 import { getCurrentUser } from 'service/connectionsAPI';
 
-// const SignUpPage = lazy(() => import('../pages/Registration/RegisterPage'));
-// const LogInPage = lazy(() => import('../pages/LogIn/LogInPage'));
-
 export const App = () => {
   const dispatch = useDispatch();
-  const isFetchingCurrentUser = useSelector(getIsFetchingCurrent);
 
   useEffect(() => {
     dispatch(getCurrentUser());
